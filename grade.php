6cba64ae47dbf3c6c114abc801ba60e263fcb935
harry3107 <31006304+harry3107@users.noreply.github.com>
2018-04-02 10:06:19 +0530

    Add files via upload

---
+++ grade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	
+	<title>online-Quiz</title>
+	
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+
+<body>
+
+	<div id="page-wrap">
+
+		<h1>Quiz for Webdevelopment</h1>
+		
+        <?php
+            
+            $answer1 = $_POST['question-1-answers'];
+            $answer2 = $_POST['question-2-answers'];
+            $answer3 = $_POST['question-3-answers'];
+            $answer4 = $_POST['question-4-answers'];
+            $answer5 = $_POST['question-5-answers'];
+        
+            $totalCorrect = 0;
+            
+            if ($answer1 == "B") { $totalCorrect++; }
+            if ($answer2 == "A") { $totalCorrect++; }
+            if ($answer3 == "C") { $totalCorrect++; }
+            if ($answer4 == "D") { $totalCorrect++; }
+			if ($answer4 == "B") { $totalCorrect++; }
+            if ($answer5) { $totalCorrect++; }
+            
+            echo "<div id='results'>$totalCorrect / 5 correct</div>";
+            
+        ?>
+	
+	</div>
+	
+	<script type="text/javascript">
+	var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+	document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+	</script>
+	<script type="text/javascript">
+	var pageTracker = _gat._getTracker("UA-68528-29");
+	pageTracker._initData();
+	pageTracker._trackPageview();
+	</script>
+
+</body>
+
+</html>
